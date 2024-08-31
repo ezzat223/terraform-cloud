af3811fb0d149e0932c94835e45b3d54553ab99b
ezzat223 <ezzat22hegazy@gmail.com>
2024-08-31 16:57:57 +0300

    modify ec2

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+data "aws_ami" "ubuntu" {
+  # This parameter specifies that the data source should return the most recent AMI that matches the filters.
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+  owners = ["099720109477"] # Canonical
+}
+
+
 resource "aws_instance" "tf-cloud" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
